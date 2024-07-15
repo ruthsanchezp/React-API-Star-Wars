0ef131192a8649429bb5960fbf82fd181f42e48e
Ruth <rh.sanchez@hotmail.com>
2024-07-15 13:20:37 -0400

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,6 +9,9 @@ function Home() {
         <h1 className="display-4 text-primary">Star Wars People</h1>
         <p className="lead">Características y detalles de los personajes de Star Wars.</p>
         <Link to="/characters" className="btn btn-primary btn-lg">View Characters</Link>
+        <p>
+        <a href="https://ruthsanchezp.github.io/react-api-star-wars/" target='_blank'>Despliege en Gihub Pages</a>
+        </p>
       </div>
     </div>
   );
